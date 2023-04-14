01610d179ba0af92a014d97ddbf546df1d9e7914
Destiny <riyajogani1@gmail.com>
2023-04-14 19:52:22 +0530

    Commit

---
+++ app/src/main/java/com/example/recycler_view/quotes.java
@@ -0,0 +1,14 @@
+package com.example.recycler_view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class quotes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quotes);
+    }
+}
